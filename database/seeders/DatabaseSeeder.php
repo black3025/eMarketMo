7824a6bec5d9dcc12ca9f2209dd2ce65b1704347
potchie <mobile.potchie@gmail.com>
2023-09-22 00:11:37 +0800

    with layout:

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        \App\Models\Role::create(
+            [
+                'name'=>"admin",
+                'role'=>1
+            ]
+        );
+        \App\Models\Role::create(
+            [
+                'name'=>"user",
+                'role'=>2
+            ]
+        );
+
+        \App\Models\User::create(
+            [
+                'name'=>'admin',
+                'password'=>'$2y$10$D8DJDvIyE6bTu1ipgioOW.jAscxPWOnlQxACoCGZflhdDV7X4q1Um',
+                'email'=>'admin@admin.com',
+                'role_id'=>1
+            ]
+        );
+        // \App\Models\User::factory(10)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+    }
+}
